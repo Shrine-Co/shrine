03f0181288e9b8bac78d68f2bf6ca122878367ce
BuildTools <unconfigured@null.spigotmc.org>
2023-11-09 00:22:15 +0530

    hello

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@nextui-org/react"
 export default function Home() {
   return (
     <div className="wrapper" style={{height:"100vh"}}>
-      <Button color="primary">Hello</Button>
+      <Button color="primary">Test</Button>
     </div>
   )
 }
